61e00a68ddf1e895d3b71c35bc91831a157752b4
parashac <parashacharya139@gmail.com>
2025-12-20 16:51:28 +0545

    models

--- gallery_app/models.py
+++ gallery_app/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Image(models.Model):
+    title= models.CharField(max_length=10)
+    album=models.ImageField(uploadto_='/gallery/album')
+    image= models.ImageField(uploadto_="/gallery")
+
