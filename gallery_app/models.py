56ee84571ff2970046e0629a4bcf8120b07e6927
parashac <parashacharya139@gmail.com>
2025-12-21 16:59:37 +0545

    admin panel

--- gallery_app/models.py
+++ gallery_app/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class Image(models.Model):
     title= models.CharField(max_length=10)
-    image= models.ImageField(upload_to ="/gallery")
+    image= models.ImageField(upload_to="gallery")
 
     def __str__(self):  # agadi pachadi double underlone ho
         return self.title
